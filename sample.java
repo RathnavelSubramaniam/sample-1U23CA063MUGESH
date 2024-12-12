30d0743236b690c8731eb9e63a8e28c0bf3b7458
1U23CA063MUGESH <142679485+1U23CA063MUGESH@users.noreply.github.com>
2024-12-12 10:04:38 +0000

    sample submitted

--- sample.java
+++ sample.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 class sample {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    Scanner Sc = new Scanner(System.in);
 
-    System.out.println("Enter name, age and salary:");
+    System.out.println("Enter the integer value:");
+    System.out.println("Enter the double value:");
+    System.out.println("Enter the string value:");
 
-    String name = myObj.nextLine();
-    int age = myObj.nextInt();
-    double salary = myObj.nextDouble();
+    String name = Sc.nextLine();
+    int age = Sc.nextInt();
+    double salary = Sc.nextDouble();
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Salary: " + salary);
